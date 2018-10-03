08c5e0dd528680b55d98e7c19f4905d58d1f44fb
manikanta.r <manikanta.r@gmail.com>
2018-10-02 23:58:33 -0400

    added js and html tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,18 @@
 var  gulp = require('gulp');
 var  connect = require('gulp-connect'); //fire browser
 var  open = require('gulp-open'); //open url in web browser
-
+var  browserify = require('browserify'); //open url in web browser
+var  reactify = require('reactify'); //open url in web browser
+var  source = require('vinyl-source-stream'); //open url in web browser
 
 var config = {
     port:9005,
     devBaseUrl:'http://localhost',
     paths:{
         html:'./src/*.html',
-        dist:'./dist'
-
+        dist:'./dist',
+        js:'./src/**/*.js',
+        mainJs:'./src/main.js',
     }
 }
 //start task to setup 
@@ -29,3 +32,30 @@ gulp.task('open',['connect'],function(){
     gulp.src('dist/index.html')
         .pipe(open({uri:config.devBaseUrl+":"+config.port+'/'}));
 });
+
+//copy html files from src to dist
+gulp.task('html',function(){
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
+});
+
+//copy html files from src to dist
+gulp.task('js',function(){
+    browserify(config.paths.mainJs)
+    .transform(reactify)
+    .bundle()
+    .on('error',console.error.bind(console))
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest( config.paths.dist+"/scripts") )   
+    .pipe(connect.reload());
+});
+
+
+gulp.task('default',['html','open','js','watch']);
+
+
+gulp.task('watch',function(){
+    gulp.watch(config.paths.html,['html']);
+    gulp.watch(config.paths.js,['js']);
+})
